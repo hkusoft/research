c997340f99ae02f28ac0a25b9ecb3c1d6fb5ddd1
David Kou <hkusoft@gmail.com>
2018-03-15 18:16:19 +0800

    Add react-tabs and update renderTabs()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 class App extends Component {
   render() {
@@ -13,9 +14,30 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        {this.renderMainTabs()}
       </div>
     );
   }
+
+  renderMainTabs = () => {
+    console.log("Rendering Tabs");
+    return (
+      <Tabs>
+        <TabList>
+          <Tab>Title 1</Tab>
+          <Tab>Title 2</Tab>
+        </TabList>
+
+        <TabPanel>
+          <h2>Any content 1</h2>
+        </TabPanel>
+        <TabPanel>
+          <h2>Any content 2</h2>
+        </TabPanel>
+      </Tabs>
+    );
+  };
 }
 
 export default App;
