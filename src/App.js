d184afdae7029e613e6530f22cb7434923413221
David Kou <hkusoft@gmail.com>
2018-03-20 17:33:12 +0800

    Add react-vertical-timeline component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import ResearchTimeline from "./components/ResearchTimeline";
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          
+          <ResearchTimeline />
           <h1 className="App-title">Welcome to David's Research Page</h1>
         </header>
-        <p className="App-intro">
+        {/* <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-        {this.renderMainTabs()}
+        {this.renderMainTabs()} */}
       </div>
     );
   }
